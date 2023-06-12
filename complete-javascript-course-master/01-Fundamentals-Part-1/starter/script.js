20d73fe4b91793e75818f5eb1c5c602fbdb0f4ff
Shahf <shahferoz_Amier@hotmail.com>
2023-06-12 10:31:18 +0200

    Week1
    StarterFiles

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,15 @@
+let js ='amazing';
+console.log(40+8+23-10);
+
+console.log("Jonas");
+
+let firstName = "Jonas";
+console.log(firstName)
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob= 'Teacher';
+
+console.log(MyFirstJob);
