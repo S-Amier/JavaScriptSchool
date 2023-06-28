e9440e9c4bd0735e7b225140fc14210a34f6b7d9
Shahf <shahferoz_Amier@hotmail.com>
2023-06-28 11:42:51 +0200

    Sectie 26
    S26

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -1,6 +1,10 @@
+//LECTURE: Values and Variables
 const country = "Netherlands";
 const continent = "Europe";
 var population = "18000000";
+
+//LECTURE: Data Types
+//LECTURE: let, const and var
 const isIsland = false;
 const language = "Dutch";
 
@@ -23,6 +27,7 @@ console.log(population > finlandPopulation);
 const averagePopulation = 33000000;
 console.log(population > averagePopulation);
 
+//LECTURE: Strings and Template Literals
 const description = (country + " is in " + continent + " and it's " + population + " people speak " + language)
 console.log(description);
 
@@ -30,10 +35,45 @@ console.log(`${country} is in ${continent} and its ${population} people speak ${
 
 console.log(`${country} population is ${averagePopulation - population} below average`);
 
+//LECTURE: Taking Decisions: if / else Statements
 if (population >= 33000000){
     console.log(`${country} population is above average`)
 }  else {
     console.log(`${country} population is ${averagePopulation - population} below average`);
 }
 
+//LECTURE: Type Conversion and Coercion
+let randomNumberA ='9' - '5'; // 4
+console.log(randomNumberA);
+let randomNumberB ='19' - '13' + '17'; // 617
+console.log(randomNumberB);
+let randomNumberC ='19' - '13' + 17; // 23
+console.log(randomNumberC);
+let randomNumberD ='123' < 57; // false
+console.log(randomNumberD);
+let randomNumberE = 5 + 6 + '4' + 9 - 4 - 2; // 1143
+console.log(randomNumberE);
 
+//LECTURE: Equality Operators: == vs. ===
+/*const numNeightbours = Number(prompt("How many neighbour countries does your country have?"));
+
+if (numNeightbours === 1){
+    console.log("Only 1 border!")
+} else if (numNeightbours >= 1){
+    console.log("More than 1 border")
+} else{
+    console.log("No borders")
+}
+*/
+
+//LECTURE: Logical Operators
+
+const speaksEnglish = true; //A
+const lessThan50million = true; //B
+const isAnIsland = false; //C
+
+if(speaksEnglish && lessThan50million && !isAnIsland){
+    console.log("You should live in the Netherlands");
+} else {
+    console.log("Netherlands does not meet your criteria :(")
+}
--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -134,7 +134,7 @@ if (birthYear <= 2000) {
     century = 21;
 }
 console.log(century);
-*/
+
 
 // type conversion
 const inputYear = "1991";
@@ -154,3 +154,70 @@ console.log("23" / "2");
 let n = "1" + 1; // string 11
 n = n - 1;  // number 10
 console.log(n);
+
+// 5 falsy values: 0, "", undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 100;
+if (money) {
+    console.log("Don't spend it all ;)");
+} else{
+    console.log("You should get a job!");
+}
+
+let height =  123;
+if(height){
+    console.log("YAY!! Height is defined");
+} else {
+    console.log("Height is UNDEFINED");
+}
+
+
+const age = 18;
+if (age === 18) console.log("You just became an adult :D(strict)");
+
+if (age == 18) console.log("You just became an adult :D(loose)");
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) { // "22 === 23 -> FALSE
+    console.log("Cool! 23 is an amazing number!");
+} else if(favourite === 7) {
+    console.log("7 is also a cool number")
+} else if (favourite === 9){
+    console.log("9 is also a cool number")
+}else {
+    console.log("Number is not 23 or 7 or 9")
+}
+
+if (favourite !== 23) console.log("Why not 23?");
+*/
+
+const hasDriversLicense = true; // A
+const hasGoodVision = true; //B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+if (hasDriversLicense && hasGoodVision) {
+    console.log("sarah is able to drive!");
+} else {
+    console.log("Someone else should drive...");
+}
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log("Sarah is able to drive!");
+} else {
+    console.log("Someone else should drive...");
+}
