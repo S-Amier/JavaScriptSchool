0476cfce706fae0a2abc80aacccd7aa02cc21ad1
Shahf <shahferoz_Amier@hotmail.com>
2023-09-04 10:54:41 +0200

    Four of a kind en Yahtzee functies toegevoegd
    JS yahtzee

--- JS yahtzee/index.js
+++ JS yahtzee/index.js
@@ -1,23 +1,72 @@
 // variabele maken waar 5 getallen in zitten. 
-let dice = [1, 5, 1, 2, 3];
+const diceArray = [1, 5, 1, 2, 3];
+//console.log(countingScore());
+console.log(generateDice());
 
-let onesScore = document.getElementById("ones");
+
+
+const onesScore = document.getElementById("klik");
 onesScore.addEventListener("click", function() {
-  this.innerText = calculateScoreForNumber(1);
+  ones.innerText = calculateScoreForNumber(1);
 })
 
-let changeScore = document.getElementById("change");
-changeScore.addEventListener("click", function() {
-  this.innerText = calculateScoreForChange();
+const twosScore = document.getElementById("klik");
+twosScore.addEventListener("click", function() {
+  twos.innerText = calculateScoreForNumber(2);
 })
+
+const threesScore = document.getElementById("klik");
+threesScore.addEventListener("click", function() {
+  threes.innerText = calculateScoreForNumber(3);
+})
+
+const foursScore = document.getElementById("klik");
+foursScore.addEventListener("click", function() {
+  fours.innerText = calculateScoreForNumber(4);
+})
+
+const fivesScore = document.getElementById("klik");
+fivesScore.addEventListener("click", function() {
+  fives.innerText = calculateScoreForNumber(5);
+})
+
+const sixesScore = document.getElementById("klik");
+sixesScore.addEventListener("click", function() {
+  sixes.innerText = calculateScoreForNumber(6);
+})
+
+const chanceScore = document.getElementById("klik");
+chanceScore.addEventListener("click", function() {
+  chance.innerText = calculateScoreForChance();
+})
+
+const fourOfaKindScore = document.getElementById("klik");
+fourOfaKindScore.addEventListener("click", function(){
+  fourOfaKind.innerText = calculateFourOfaKind();
+})
+
+const yahtzeeScore = document.getElementById("klik");
+yahtzeeScore.addEventListener("click", function() {
+  yahtzee.innerText = calculateYahtzee();
+})
+
+
+// het woordje functie, functie naam (argumenten) {scope}
+function generateDice() {
+  for (let i = 0; i < diceArray.length; i++) {
+      //maakt gelijk aan een afgerond willekeurig getal tussen de 1 en 6
+      diceArray[i] = Math.floor(Math.random() * 6) + 1;
+  }
+}
+    //console.log(`index: ${index} diceValue: ${dice[index]}`);
 
 
 function countNumber(num) {
   let result = 0;
   // voor iedere dobbelsteen
-  for (let index = 0; index < dice.length; index++ ) {
+  for (let index = 0; index < diceArray.length; index++ ) {
   // als de dobbelsteen 2 is
-    if (dice[index] === num) {
+    if (diceArray[index] === num) {
       result++; // result = result + 1; result += 1;
     }
   }
@@ -29,24 +78,78 @@ function calculateScoreForNumber(num) {
   return countNumber(num) * num;
 }
 
-function calculateScoreForChange(){
+function calculateScoreForChance(){
   let result = 0;
-  for (let index = 0; index < dice.length; index++ ) {
-    result = result + dice[index];
+  for (let index = 0; index < diceArray.length; index++ ) {
+    result = result + diceArray[index];
   }
 
   return result;  
 }
+
+// Bereken Four of a Kind
+function calculateFourOfaKind(){
+  let result = 0;
+  if (diceArray === 4){
+    result++;
+  }// else {
+    //result = false;
+    return result
+}
+
+// Bereken Yahtzee
+function calculateYahtzee(){
+  let result = 0;
+  if (diceArray === 5){
+    result = 50;
+  }// else {
+    //result = false;
+    return result;
+}
+
+  
+
+
+/*
+function countingScore(){
+  const eyes = [
+  countEyeAmount(1),
+  countEyeAmount(2),
+  countEyeAmount(3),
+  countEyeAmount(4),
+  countEyeAmount(5),
+  countEyeAmount(6)
+  ];
+
+  return eyes;
+
+}
+
+function countEyeAmount(number){
+  let count = 0;
+  for(let i = 0; i < diceArray.length; i++){
+    if(diceArray[i] === number) count++;
+  }
+  return count;
+}*/
+
+
+
+
+
+
+
+
 
 /* Mogelijk volgende stappen:
 
 - functie die dice array met random getallen (tussen 1 en 6) vult
 - Als ik op de 'gooien' button klik, dan moet het array worden gevuld met random waarden
 - Zorg dat de waarden in het dice array op het scherm getoond worden (dat zijn dan de ogen op de dobbelstenen)
-- Maak fucnties om score te berekenen (en dus soms om te bepalen of datgene gegooid is):
+- Maak functies om score te berekenen (en dus soms om te bepalen of datgene gegooid is):
   - 1 t/m 6
   - 4 of a kind
-  - yathzee
+  - yahtzee
   - full house
   - kleine straat
   - grote straat
